127c119720b4192e55c70b1aada74354898364ce
Hari <hariharanb39@gmail.com>
2026-04-28 13:21:30 +0530

    Added RequestParam for field and level

--- vocabulary-system/src/main/java/com/vocabulary/vocabulary_system/VocabularyController.java
+++ vocabulary-system/src/main/java/com/vocabulary/vocabulary_system/VocabularyController.java
@@ -1,9 +1,6 @@
 package com.vocabulary.vocabulary_system;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/vocabulary")
@@ -16,8 +13,14 @@ public class VocabularyController {
     }
 
     @GetMapping("/search/{word}")
-    public VocabularyResponse searchWord(@PathVariable String word)
+    public VocabularyResponse searchWord(@PathVariable String word,
+                                         @RequestParam(required=false,defaultValue = "general")String field,
+                                         @RequestParam(required=false,defaultValue = "beginner")String level)
     {
-        return new VocabularyResponse(word,"Searching for:"+word);
+        String message = "Searching for: " + word +
+                " in field: " + field +
+                " at level: " + level;
+
+        return new VocabularyResponse(word,message);
     }
 }
